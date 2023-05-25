9615882b05cbdef3ec2bc920b03d5bd13bed81d0
YH TAY <yhtay2000@gmail.com>
2023-05-25 16:33:35 +0800

    new 2

--- app/src/main/java/com/example/projectapplication/MainActivity3.java
+++ app/src/main/java/com/example/projectapplication/MainActivity3.java
@@ -2,6 +2,7 @@ package com.example.projectapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity3 extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Intent intent = new Intent();
     }
 }
