d6d1381b4a0c279624cc4d4ffa0a56683b861aa3
Tan <tsk981229@gmail.com>
2023-05-31 22:20:20 +0800

    Tan

--- app/src/main/java/com/example/projectapplication/GuestAdd.java
+++ app/src/main/java/com/example/projectapplication/GuestAdd.java
@@ -3,12 +3,38 @@ package com.example.projectapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GuestAdd extends AppCompatActivity {
 
+    EditText name, age, gender, address, phone;
+    Button submit_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_guest);
+
+        name = findViewById(R.id.name_input);
+        age = findViewById(R.id.age_input);
+        gender = findViewById(R.id.gender_input);
+        address = findViewById(R.id.address_input);
+        phone = findViewById(R.id.phone_input);
+        submit_button = findViewById(R.id.submit_button);
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabase myDB = new MyDatabase(GuestAdd.this);
+                myDB.addGuest(
+                        name.getText().toString().trim(),
+                        gender.getText().toString().trim(),
+                        Integer.valueOf(age.getText().toString().trim()),
+                        phone.getText().toString().trim(),
+                        address.getText().toString().trim()
+                );
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectapplication/MyDatabase.java
+++ app/src/main/java/com/example/projectapplication/MyDatabase.java
@@ -1,9 +1,11 @@
 package com.example.projectapplication;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -29,12 +31,12 @@ public class MyDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME +
-                       " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                       " ( " + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                        COLUMN_NAME + " TEXT, " +
-                       COLUMN_GENDER + "TEXT, " +
-                       COLUMN_AGE + "INTEGER, " +
-                       COLUMN_ADDRESS + "TEXT, " +
-                       COLUMN_PHONE + "TEXT);";
+                       COLUMN_GENDER + " TEXT, " +
+                       COLUMN_AGE + " INTEGER, " +
+                       COLUMN_ADDRESS + " TEXT, " +
+                       COLUMN_PHONE + " TEXT );";
         db.execSQL(query);
     }
 
@@ -42,5 +44,23 @@ public class MyDatabase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addGuest(String name, String gender, int age, String address, String phone){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_GENDER, gender);
+        cv.put(COLUMN_AGE, age);
+        cv.put(COLUMN_ADDRESS, address);
+        cv.put(COLUMN_PHONE, phone);
+
+        long result = db.insert(TABLE_NAME, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Fail To Insert", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Insert Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
