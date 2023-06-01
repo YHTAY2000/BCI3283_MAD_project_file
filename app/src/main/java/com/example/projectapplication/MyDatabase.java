7bab629dd1a6ec306185a3cbc4e9ff789bd6f983
Tan <tsk981229@gmail.com>
2023-06-01 10:55:33 +0800

    Tan

--- app/src/main/java/com/example/projectapplication/MyDatabase.java
+++ app/src/main/java/com/example/projectapplication/MyDatabase.java
@@ -3,6 +3,7 @@ package com.example.projectapplication;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -62,5 +63,16 @@ public class MyDatabase extends SQLiteOpenHelper {
         }else{
             Toast.makeText(context, "Insert Successfully", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
     }
 }
