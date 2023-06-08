4b32ff843bfcf9950d0f32691d8efbba178fe298
Bryan Tan <bryantan8858@gmail.com>
2023-06-08 17:28:23 +0800

    add event and database (bryan)

--- app/src/main/java/com/example/projectapplication/Event_Add.java
+++ app/src/main/java/com/example/projectapplication/Event_Add.java
@@ -5,63 +5,86 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.Calendar;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Event_Add extends AppCompatActivity {
 
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private EditText editTextDate;
-    private EditText editTextTime;
-
-    private Calendar calendar;
-    private Button uploadBTN;
     private ImageView eventImage;
+    private Button uploadBTN;
+    private EditText eventNameText;
+    private EditText eventOrganizerText;
+    private EditText date;
+    private EditText time;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Calendar calendar;
+    private MyDatabase db;
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_add);
 
-        uploadBTN = findViewById(R.id.uploadButton);
+        //Initialize views
         eventImage = findViewById(R.id.eventImage);
+        uploadBTN = findViewById(R.id.uploadButton);
+        eventNameText = findViewById(R.id.eventNameTxt);
+        eventOrganizerText = findViewById(R.id.organizerNameTxt);
+        date = findViewById(R.id.dateText);
+        time = findViewById(R.id.timeText);
+        calendar = Calendar.getInstance();
 
+        //Initialize database helper
+        db = new MyDatabase(this);
+
+        //set click listener for upload button
         uploadBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openFileChooser();
+                openImagePicker();
             }
         });
 
-        editTextDate = findViewById(R.id.dateText);
-        editTextTime = findViewById(R.id.timeText);
-
-        calendar = Calendar.getInstance();
-
-        editTextDate.setOnClickListener(new View.OnClickListener() {
+        //show the calender picker and time picker
+        date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePicker();
             }
         });
-
-        editTextTime.setOnClickListener(new View.OnClickListener() {
+        time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTimePicker();
             }
         });
+
+        //handles the add event
+        Button addButton = findViewById(R.id.addEventBTN);
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addEvent();
+            }
+        });
     }
 
-    private void openFileChooser() {
+    private void openImagePicker() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -73,10 +96,59 @@ public class Event_Add extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
-            eventImage.setImageURI(imageUri);
+            selectedImageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                eventImage.setImageBitmap(bitmap);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+
         }
     }
+
+    private void addEvent() {
+        //Get entered event data
+        String event_name = eventNameText.getText().toString().trim();
+        String event_organizer = eventOrganizerText.getText().toString().trim();
+        String event_date = date.getText().toString().trim();
+        String event_time = time.getText().toString().trim();
+
+        // Check if an image is selected
+        if (selectedImageUri == null) {
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Convert the image to a byte array
+        byte[] imageBytes = convertImageToByteArray(selectedImageUri);
+
+        // Save the event to the database
+        long eventId = db.addEvent(event_name, event_organizer, event_date, event_time, imageBytes);
+        if (eventId != -1) {
+            Toast.makeText(this, "Event added", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(this, "Error: Failed to add event", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private byte[] convertImageToByteArray(Uri imageUri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+            }
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     private void showDatePicker() {
         int year = calendar.get(Calendar.YEAR);
@@ -89,7 +161,7 @@ public class Event_Add extends AppCompatActivity {
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         // Update the EditText field with the selected date
                         String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
-                        editTextDate.setText(selectedDate);
+                        date.setText(selectedDate);
                     }
                 }, year, month, day);
 
@@ -118,10 +190,11 @@ public class Event_Add extends AppCompatActivity {
 
                         // Update the EditText field with the selected time
                         String selectedTime = String.format("%02d:%02d %s", hourOfDay12, minute, format);
-                        editTextTime.setText(selectedTime);
+                        time.setText(selectedTime);
                     }
                 }, hour, minute, false); // Set the last parameter to false to display in 12-hour format
 
         timePickerDialog.show();
     }
+
 }
--- app/src/main/java/com/example/projectapplication/MyDatabase.java
+++ app/src/main/java/com/example/projectapplication/MyDatabase.java
@@ -6,11 +6,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-
-import org.w3c.dom.Text;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
@@ -32,12 +31,13 @@ public class MyDatabase extends SQLiteOpenHelper {
     private static final String COLUMN_NAME2 = "guest_name";
 
     //Event details Table
-    public static final String TABLE_NAME3 = "event";
-    public static final String COLUMN_ID3 = "event_id";
-    public static final String COLUMN_EVENT = "event_name";
-    public static final String COLUMN_ORGANIZER = "event_organizer";
-    public static final String COLUMN_DATE = "event_date";
-    public static final String COLUMN_TIME = "event_time";
+    private static final String TABLE_NAME3 = "event";
+    private static final String COLUMN_ID3 = "event_id";
+    private static final String COLUMN_EVENT = "event_name";
+    private static final String COLUMN_ORGANIZER = "event_organizer";
+    private static final String COLUMN_DATE = "event_date";
+    private static final String COLUMN_TIME = "event_time";
+    private static final String COLUMN_IMAGE = "event_image";
 
     public MyDatabase(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -58,14 +58,24 @@ public class MyDatabase extends SQLiteOpenHelper {
                 " ( " + COLUMN_ID2 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_NAME2 + " TEXT ); ";
 
+        String insertEvent = "CREATE TABLE " + TABLE_NAME3 +
+                "( " + COLUMN_ID3 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_EVENT + " TEXT, " +
+                COLUMN_ORGANIZER + " TEXT, " +
+                COLUMN_DATE + " TEXT, " +
+                COLUMN_TIME + " TEXT," +
+                COLUMN_IMAGE + " BLOB );";
+
         db.execSQL(query);
         db.execSQL(query2);
+        db.execSQL(insertEvent);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME2);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME3);
         onCreate(db);
     }
 
@@ -162,5 +172,20 @@ public class MyDatabase extends SQLiteOpenHelper {
             cursor = db.rawQuery(query, null);
         }
         return cursor;
+    }
+
+    //insert event
+    public long addEvent(String event, String organizer, String date, String time, byte[] image){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_EVENT, event);
+        values.put(COLUMN_ORGANIZER, organizer);
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_TIME, time);
+        values.put(COLUMN_IMAGE, image);
+        long eventId = db.insert("event", null, values);
+        db.close();
+
+        return eventId;
     }
 }
