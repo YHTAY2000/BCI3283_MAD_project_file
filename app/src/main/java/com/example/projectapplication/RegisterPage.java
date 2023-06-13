f95b9eddc01b1e212b3f379a2d8cf7300c66c17d
Dee <dhurgashini99@yahoo.com>
2023-06-13 19:39:22 +0800

    dee2

--- app/src/main/java/com/example/projectapplication/RegisterPage.java
+++ app/src/main/java/com/example/projectapplication/RegisterPage.java
@@ -1,4 +1,18 @@
 package com.example.projectapplication;
 
-public class RegisterPage {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class RegisterPage extends AppCompatActivity {
+    TextInputEditText editTextTextEmailAddress, editText2,editText1;
+            Button register;
+
+            protected void onCreate(Bundle savedInstanceState){
+                super.onCreate(savedInstanceState);
+            }
+
 }
